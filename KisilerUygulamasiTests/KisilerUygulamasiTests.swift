8643487ca4f3e2452f24426a4fd271975ae75ca3
Eren-Unal <bilalunal24@gmail.com>
2024-09-22 14:18:43 +0300

    Initial Commit

---
+++ KisilerUygulamasiTests/KisilerUygulamasiTests.swift
@@ -0,0 +1,17 @@
+//
+//  KisilerUygulamasiTests.swift
+//  KisilerUygulamasiTests
+//
+//  Created by Eren on 22.09.2024.
+//
+
+import Testing
+@testable import KisilerUygulamasi
+
+struct KisilerUygulamasiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
